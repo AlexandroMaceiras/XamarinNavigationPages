5e1720c2eea2f4c95a7e40116795ab08b9714dbe
Alexandro Maceiras <alexandro@maceiras.com.br>
2020-06-21 21:46:09 -0300

    Adicionar arquivos de projeto.

---
+++ XamarinNavigationPages/TipoPagina/Navigation/Pagina1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinNavigationPages.TipoPagina.Navigation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina1 : ContentPage
+    {
+        public Pagina1()
+        {
+            InitializeComponent();
+        }
+        private void MudarPagina2(object o, EventArgs e)
+        {
+            //App.Current.MainPage = new XamarinNavigationPages.TipoPagina.Navigation.Pagina2(); //Esse não mantem o histórico de quem chamou ela.
+            Navigation.PushAsync(new Pagina2()); //Esse MANTEM o histórico de quem chamou ela.
+        }
+    }
+}
